d2e77937551407645e8eb25d198cf8eca3600524
Esio Freitas <esio.gustavo@gmail.com>
2020-05-26 14:35:22 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes';
+import GlobalStyles from './assets/style/global';
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
