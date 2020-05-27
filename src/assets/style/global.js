c0aab2e29ca74d470c919d3563b4e3746303a960
EsioFreitas <esio.gustavo@gmail.com>
2020-05-27 07:34:44 -0300

    Add fonts

--- src/assets/style/global.js
+++ src/assets/style/global.js
@@ -7,4 +7,12 @@ export default createGlobalStyle`
   outline: 0;
   box-sizing: border-box;
 }
+
+h1, h2, h3, h4  {
+  font-family: 'Comfortaa', cursive;
+}
+
+p {
+  font-family: 'Open Sans', sans-serif;
+}
 `;
